f7097c9c15aae741c6e30b1f8008ff935da75708
Latif-Karimi <114200825+Latif-Karimi@users.noreply.github.com>
2023-04-18 17:24:44 -0400

    updated
    added esmodeule fix

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ import cors from "cors"
 import productRoutes from "./Back/routes/productRoute.js"
 import bodyParser from'body-parser';
 import path from "path"
+import {fileURLToPath} from 'url';
 
 
 //configure env
 dotenv.config();
+
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //database confiq
 connectDB();
